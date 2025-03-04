3d576907e6892c68079cedc82190f7ee0588a8d5
JonathonJulian <j@jonathonfritz.com>
2025-03-04 05:06:46 -0800

    Update base URL for custom domain support

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/resume/', // Set base URL to match the GitHub Pages repository name
+  base: '/', // Set base URL to root for custom domain
 })
